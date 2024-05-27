60835ca85d94bb2e201e42a0799959eee5af63da
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-05-27 19:41:22 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas,setTareas] = useState(["hacerTarea", "otra tarea"])
+	const [nuevaTarea, setNuevaTarea] = useState([""]);
+
+
+	const handleTAREAnUEVA = (event) =>{
+		setTareanueva(event.target.value)
+	}
+	
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		setTareas
+		}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	<div className="w-50 m-auto mt-5">
+		<form onSubmit = {handleTareas} >
+			<div className="mb-3">
+				<label for="exampleInputEmail1" className="form-label">Tarea</label>
+				<input onChange={handleTareas} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+				<div id="tasklHelp" className="form-text">pon la tarea.</div>
+			</div>
+			<button type="submit" className="btn btn-primary">Submit</button>
+			
+		</form>
+
+		<ul className=" w-50 m-auto">
+			{tareas.map((item,index) => (
+				<li key={index}>{item} 
+				<button onClick={ ()=> handleDelete(index)} type></button>
+				</li>
+			))}
+		</ul>
+	</div>
 	);
 };
 
